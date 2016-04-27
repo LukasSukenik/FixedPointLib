bb5b793317705036cef703672e3fe041e666fe18
Lukas Sukenik <xsukenik@nymfe97.fi.muni.cz>
2016-04-27 16:54:04 +0200

    first

---
+++ fixed.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Fixed {
+public:
+
+    Fixed& operator=(Fixed o);
+
+    Fixed operator+(Fixed o);
+    Fixed operator-(Fixed o);
+
+    Fixed operator+();
+    Fixed operator-();
+
+    Fixed operator++(int); // postfix
+    Fixed operator--(int );
+
+    Fixed& operator++(); // prefix
+    Fixed& operator--();
+};
